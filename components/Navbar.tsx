92ebe26c6da27aad9f9b8cc6b39b8fae31e99b0d
Mayuran Ganesathas <mayuran.ganesathas@gmail.com>
2021-10-17 09:39:50 -0400

    mg-WIP

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from "./UserProfile";
+import HrProfile from "./HrProfile";
+
+const NavBar = () => {
+  return (
+    <div>
+      <UserProfile />
+      <HrProfile />
+    </div>
+  );
+};
