a2151165eb507f3bf0e5a84a42388c3edea0f84c
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-19 06:58:10 -0400

    candidate layout (no arrays yet)

---
+++ components/dashView/CandidateCount.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { CandidateCount, CandidateCountProps } from "./CandidateCount";
+
+export default {
+  title: "UI/CandidateCount",
+  component: CandidateCount,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<CandidateCountProps> = (args) => (
+  <CandidateCount {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  totalCandidateIcon: "/images/candidates.png",
+};
--- components/dashView/CandidateCount.tsx
+++ components/dashView/CandidateCount.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
-export interface CandidateCountProps {}
+export interface CandidateCountProps {
+  totalCandidateIcon: string;
+}
 
-export const CandidateCount: React.FC<CandidateCountProps> = () => {
-  return <div></div>;
+export const CandidateCount: React.FC<CandidateCountProps> = ({
+  totalCandidateIcon,
+}) => {
+  return (
+    <div className={"grid grid-cols-6 w-48"}>
+      <div
+        className={"flex items-center justify-center col-start-1 col-span-2"}
+      >
+        <img src={totalCandidateIcon} className="" width="50" height="50" />
+      </div>
+      <div
+        className={
+          "flex items-center justify-center text-xs col-start-3 col-span-1"
+        }
+      >
+        {" "}
+        43
+      </div>
+
+      <div
+        className={
+          "flex items-center justify-center text-xs font-bold col-start-4 col-span-3"
+        }
+      >
+        {" "}
+        Candidates
+      </div>
+    </div>
+  );
 };
