31336acf6a4b67493d2871b2d8bcafa8dc11c3b4
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-22 14:24:22 -0400

    Onclick for email and linkedinBut

--- components/dashView/CompTooltip.tsx
+++ components/dashView/CompTooltip.tsx
@@ -4,6 +4,7 @@ export interface CompTooltipProps {
   companyLocation: string;
   numEmployees: any;
   userHrCompanyWebsite: string;
+  onClick?: () => void;
 }
 
 export const CompTooltip: React.FC<CompTooltipProps> = ({
@@ -11,15 +12,32 @@ export const CompTooltip: React.FC<CompTooltipProps> = ({
   companyLocation,
   numEmployees,
   userHrCompanyWebsite,
+  onClick,
 }) => {
   return (
     <div
-      className={"h-94 w-64 bg-white border-2 border-gray-200 drop-shadow-md"}
+      className={
+        "w-96 bg-white border border-black drop-shadow-md py-2 px-4 rounded-tr-3xl rounded-b-3xl"
+      }
     >
-      <div> {companyName}</div>
-      <div> Location: {companyLocation}</div>
-      <div> Number of Employees: {numEmployees}</div>
-      <div> {userHrCompanyWebsite}</div>
+      <div className={"pb-2"}> {companyName}</div>
+      <div className={"text-xs"}>
+        <div className={"grid grid-cols-6"}>
+          <div className={"text-gray-500 w-14 col-start-1 py-1"}>
+            {" "}
+            Location:
+          </div>
+          <div className={"col-start-2 col-span-5 pl-3"}>{companyLocation}</div>
+        </div>
+        <div className={"grid grid-cols-3"}>
+          <div className={"text-gray-500 w-36"}>Number of Employees:</div>
+          <div className={"col-start-2 pl-6"}>{numEmployees}</div>
+        </div>
+        <div className={"pt-1 pb-2 underline italic"} onClick={onClick}>
+          {" "}
+          {userHrCompanyWebsite}
+        </div>
+      </div>
     </div>
   );
 };
