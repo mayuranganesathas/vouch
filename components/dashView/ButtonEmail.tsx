88a98f7e07a7a7cfc431e8f275537e790d23c465
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-21 21:15:11 -0400

    rounded buttons

--- components/dashView/ButtonEmail.tsx
+++ components/dashView/ButtonEmail.tsx
@@ -1,29 +1,26 @@
 import React from "react";
 export interface ButtonEmailProps {
   backgroundColour: "white";
-  buttonType: "rounded";
   disabled?: boolean;
 }
 
 export const ButtonEmail: React.FC<ButtonEmailProps> = ({
   backgroundColour,
-  buttonType,
   disabled,
 }) => {
   return (
     <button
       disabled={disabled}
       type="button"
-      className={`bg-gradient-to-b px-8 py-2 text-red border border-gray-300 w-40 h-auto
+      className={`bg-gradient-to-b px-8 py-2 text-red border border-gray-300 w-40 h-auto rounded
       ${disabled ? "bg-gray-400 opacity-25" : backgroundColour}
-      ${disabled ? "rounded-none" : buttonType}
-      ${disabled ? "" : "active:border-b-2"}
+
       ${disabled ? "cursor-default" : "cursor-pointer"}
       `}
     >
       <img
         src="./images/email.png"
-        className={"flex justify-center items-center"}
+        className={"flex justify-center items-center p-3"}
       />
     </button>
   );
--- components/dashView/ButtonLinkedin.tsx
+++ components/dashView/ButtonLinkedin.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 export interface ButtonLinkedinProps {
   backgroundColour: "white";
-  buttonType: "rounded";
   disabled?: boolean;
 }
 
 export const ButtonLinkedin: React.FC<ButtonLinkedinProps> = ({
   backgroundColour,
-  buttonType,
   disabled,
 }) => {
   return (
     <button
       disabled={disabled}
       type="button"
-      className={`bg-gradient-to-b px-8 py-2 text-red border border-gray-300 w-40 h-auto
+      className={`bg-gradient-to-b px-8 py-2 text-red border border-gray-300 w-40 h-auto rounded
       ${disabled ? "bg-gray-400 opacity-25" : backgroundColour}
-      ${disabled ? "rounded-none" : buttonType}
-      ${disabled ? "" : "active:border-b-2"}
       ${disabled ? "cursor-default" : "cursor-pointer"}
       `}
     >
