965ae0cfa44f96b7505ef85b1fdb114e8b9ff795
Mayuran Ganesathas <mayuran.ganesathas@gmail.com>
2021-10-23 01:05:49 -0400

    mg-update

--- components/dashView/VouchCTAModal.tsx
+++ components/dashView/VouchCTAModal.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
-
+import { BeakerIcon, XIcon } from "@heroicons/react/solid";
+import { ButtonVouch } from "../buttonVouch";
+import { SearchFilterDash } from "./searchFilterDash";
 // ref http://reactcommunity.org/react-modal/
 //ref https://github.com/tailwindlabs/heroicons
 export interface VouchCTAModalProps {
@@ -20,6 +22,31 @@ const VouchCTAModal = ({ modalIsOpen, closeModal }: VouchCTAModalProps) => {
     },
   };
 
+  const [formVaildation, setFormValidation] = useState(false);
+  const [email, setEmail] = useState("");
+  const [positionTitle, setPositionTitle] = useState("");
+  const [interviewStage, setInterviewStage] = useState("");
+  const [positionLevel, setPositionLevel] = useState("");
+  const [salaryRange, setSalaryRange] = useState("");
+  const [jobPostingLink, setJobPostingLink] = useState("");
+  const [standOutSkill1, setStandOutSkill1] = useState("");
+  const [standOutSkill2, setStandOutSkill2] = useState("");
+  const [standOutSkill3, setStandOutSkill3] = useState("");
+  const [standOutSkill4, setStandOutSkill4] = useState("");
+  const [standOutSkill5, setStandOutSkill5] = useState("");
+
+  const dropDownArray = ["test1", "test2", "test3", "test4", "test5"];
+  //replace with DB Call
+  const emailChecker = () => {
+    //toDO Form validation, check email input to see if it includes .com / or .ca or .in etc.
+  };
+
+  const submitForm = () => {
+    //post to db
+    //clear states to 0
+    //send email to candidate
+  };
+
   return (
     <div className=" ">
       <Modal
@@ -28,16 +55,192 @@ const VouchCTAModal = ({ modalIsOpen, closeModal }: VouchCTAModalProps) => {
         style={customStyles}
         contentLabel="Test Name"
       >
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>{" "}
-        <button onClick={closeModal} className="bg-red-300">
-          close
-        </button>
+        <div>
+          <div className="flex justify-end gap-x-96">
+            <div>
+              <div className="py-2">Referred Candidate Email</div>
+              <div>
+                <input
+                  className="border-2 w-60 rounded-lg"
+                  id="guess"
+                  type="text"
+                  placeholder=" Enter Candidate Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <XIcon
+              className="h-5 w-5 text-gray-400 hover:text-red-500 cursor-pointer"
+              onClick={closeModal}
+            />
+          </div>
+          <div className="py-4">
+            <hr className="text-red-500 p-4 h-8 justify-items-end" />
+          </div>
+
+          <div className="py-4">
+            <div className="text-bold">Position Details</div>
+            <div className="text-gray-400 text-xs">
+              For the role they just interviewed for
+            </div>
+          </div>
+          <div>
+            <div className="flex-norwap">
+              <div className="py-2 text-sm">Position Title</div>
+              <SearchFilterDash
+                value={positionTitle}
+                backgroundColour="white"
+                onChange={(e) => {
+                  setPositionTitle(e.target.value);
+                }}
+                dropDownArray={dropDownArray}
+                width="wide-sm"
+              />
+            </div>
+            <div className="py-4 flex gap-8">
+              <div>
+                <div className="text-sm py-0.5">Stage Of Interview:</div>{" "}
+                <SearchFilterDash
+                  value={interviewStage}
+                  backgroundColour="white"
+                  onChange={(e) => {
+                    setInterviewStage(e.target.value);
+                  }}
+                  dropDownArray={dropDownArray}
+                  width="wide-sm"
+                />
+              </div>
+              <div>
+                <div className="text-sm py-0.5">Position Level:</div>{" "}
+                <SearchFilterDash
+                  value={positionLevel}
+                  backgroundColour="white"
+                  onChange={(e) => {
+                    setPositionLevel(e.target.value);
+                  }}
+                  dropDownArray={dropDownArray}
+                  width="wide-sm"
+                />
+              </div>
+              <div>
+                <div className="text-sm py-0.5">Salary Range:</div>{" "}
+                <SearchFilterDash
+                  value={salaryRange}
+                  backgroundColour="white"
+                  onChange={(e) => {
+                    setSalaryRange(e.target.value);
+                  }}
+                  dropDownArray={dropDownArray}
+                  width="wide-sm"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="py-4">
+            <div className="text-sm py-1">Link to job Posting</div>{" "}
+            <input
+              placeholder=" Job Posting"
+              className="border-2 w-60 rounded-lg"
+              value={jobPostingLink}
+              onChange={(e) => setJobPostingLink(e.target.value)}
+            />
+          </div>
+          <div className="py-4">
+            <div className="text-bold">Stand Out Skills</div>
+            <div className="text-gray-400 text-xs">
+              Select the top skill that impressed you the most about this
+              Candidate{" "}
+            </div>
+            <div className="flex flex-wrap justify-center gap-x-8  gap-y-6 py-6">
+              <label
+                className={
+                  " flex text-gray-400   border-2 rounded-full w-36  gap-x-0.5"
+                }
+              >
+                <input
+                  type="radio"
+                  value={dropDownArray[0]}
+                  className={"m-1.5"}
+                  name="standout"
+                  onChange={(e) => setStandOutSkill1(e.target.value)}
+                />
+                <div className="gap-x-1"> {dropDownArray[0]}</div>
+              </label>
+              <label
+                className={
+                  " flex text-gray-400   border-2 rounded-full w-36  gap-x-0.5"
+                }
+              >
+                {" "}
+                <input
+                  type="radio"
+                  value={dropDownArray[1]}
+                  className={"m-1.5"}
+                  name="standout"
+                  onChange={(e) => setStandOutSkill2(e.target.value)}
+                />
+                {dropDownArray[1]}
+              </label>
+              <label
+                className={
+                  " flex text-gray-400   border-2 rounded-full w-36  gap-x-0.5"
+                }
+              >
+                {" "}
+                <input
+                  type="radio"
+                  value={dropDownArray[2]}
+                  className={"m-1.5"}
+                  name="standout"
+                  onChange={(e) => setStandOutSkill3(e.target.value)}
+                />
+                {dropDownArray[2]}
+              </label>
+              <label
+                className={
+                  " flex  text-gray-400 border-2 rounded-full w-36  gap-x-0.5"
+                }
+              >
+                {" "}
+                <input
+                  type="radio"
+                  value={dropDownArray[3]}
+                  className={"m-1.5"}
+                  name="standout"
+                  onChange={(e) => setStandOutSkill4(e.target.value)}
+                />
+                {dropDownArray[3]}
+              </label>
+              <label
+                className={
+                  " flex  text-gray-400  border-2 rounded-full w-36 gap-x-0.5"
+                }
+              >
+                {" "}
+                <input
+                  type="radio"
+                  value={dropDownArray[4]}
+                  className={"m-1.5"}
+                  name="standout"
+                  onChange={(e) => setStandOutSkill5(e.target.value)}
+                />
+                {dropDownArray[4]}
+              </label>
+            </div>
+          </div>
+          <div className="py-1 justify-items-end">
+            <ButtonVouch
+              backgroundColour="VouchGreen"
+              textColour="white"
+              onClick={submitForm}
+              buttonType="rounded"
+              label="Refer Candidate!"
+              disabled={formVaildation ? false : true}
+            />
+            {/* {setFormValidation( check forms ? true: false)} */}
+          </div>
+        </div>
       </Modal>{" "}
     </div>
   );
--- components/dashView/searchFilterDash.tsx
+++ components/dashView/searchFilterDash.tsx
@@ -3,24 +3,48 @@ import React from "react";
 export interface SearchFilterDashProps {
   backgroundColour: "VouchGreen" | "white";
   dropDownArray: any[];
+  value: any;
+  onChange: (e) => void;
+  width?: "wide-md" | "wide-lg" | "wide-sm";
 }
 
 export const SearchFilterDash: React.FC<SearchFilterDashProps> = ({
   backgroundColour,
   dropDownArray,
+  value,
+  onChange,
+  width,
 }) => {
   let backgroundStyles;
   switch (backgroundColour) {
     case "VouchGreen":
-      backgroundStyles = "bg-VouchGreen border-2 border-gray-400 px-8";
+      backgroundStyles = "bg-VouchGreen border-1 border-gray-200 px-8";
       break;
     case "white":
-      backgroundStyles = "bg-gray-50 border-2 border-gray-400 px-8";
+      backgroundStyles = "bg-white border-2 border-gray-200 px-8";
+      break;
+  }
+  let buttonWidth;
+  switch (width) {
+    case "wide-lg":
+      buttonWidth = "w-96";
+      break;
+    case "wide-md":
+      buttonWidth = "w-60";
+      break;
+    case "wide-sm":
+      buttonWidth = "w-40";
       break;
   }
   return (
     <form>
-      <select className={backgroundStyles}>
+      <select
+        className={`${backgroundStyles} ${buttonWidth} rounded-md `}
+        name="Select From List"
+        id="Select From List"
+        value={value}
+        onChange={onChange}
+      >
         {dropDownArray &&
           dropDownArray
             .filter((v, i, a) => a.indexOf(v) === i)
