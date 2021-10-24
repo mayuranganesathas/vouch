65bdc621492c5c6a3b29467df01d6a33a661cac7
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-24 14:21:03 -0400

    fixed candidat count and put into page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import UserIdBar from "../components/UserIdBar";
+import WelcomeComp from "../components/dashView/WelcomeComp";
+import VouchCTA from "../components/dashView/VouchCTA";
+
+const dashBoardTest = {
+  newCandidateNumber: 14,
+  userHrFirstName: "Viv",
+  moveToCandidate: () => {},
+  numberReferred: 10,
+  numberThanks: 4,
+};
 
 const DashBoard = () => {
   return (
     <div>
-      <UserIdBar />
+      <div className={"pt-4 px-20"}>
+        <UserIdBar />
+      </div>
+      <div className={"w-full border-gray-500 border-b"}></div>
+      <div className={"px-2 grid grid-cols-2"}>
+        <div className={"pl-24 pt-10"}>
+          <WelcomeComp
+            newCandidateNumber={dashBoardTest.newCandidateNumber}
+            userHrFirstName={dashBoardTest.userHrFirstName}
+            moveToCandidates={dashBoardTest.moveToCandidate}
+          />
+        </div>
+
+        <div className="grid justify-items-end pr-40 pt-10">
+          <VouchCTA
+            numberReferred={dashBoardTest.numberReferred}
+            numberThanks={dashBoardTest.numberThanks}
+          />
+        </div>
+      </div>
     </div>
   );
 };
