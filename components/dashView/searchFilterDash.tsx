cded1dc62d06cd7f13ff3179058ac040276a0a81
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-24 20:14:03 -0400

    layout done

--- components/dashView/searchFilterDash.tsx
+++ components/dashView/searchFilterDash.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 
 export interface SearchFilterDashProps {
-  backgroundColour: "VouchGreen" | "white";
   dropDownArray: any[];
 }
 
 export const SearchFilterDash: React.FC<SearchFilterDashProps> = ({
-  backgroundColour,
   dropDownArray,
 }) => {
-  let backgroundStyles;
-  switch (backgroundColour) {
-    case "VouchGreen":
-      backgroundStyles = "bg-VouchGreen border-2 border-gray-400 px-8";
-      break;
-    case "white":
-      backgroundStyles = "bg-gray-50 border-2 border-gray-400 px-8";
-      break;
-  }
   return (
     <form>
-      <select className={backgroundStyles}>
+      <select className={"bg-white border border-gray-500"}>
         {dropDownArray &&
           dropDownArray
             .filter((v, i, a) => a.indexOf(v) === i)
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,6 +4,7 @@ import WelcomeComp from "../components/dashView/WelcomeComp";
 import VouchCTA from "../components/dashView/VouchCTA";
 import { CandidateCount } from "../components/dashView/CandidateCount";
 import { SearchFilterDash } from "../components/dashView/SearchFilterDash";
+import { CandidateTile } from "../components/dashView/CandidateTile";
 
 const dashBoardTest = {
   newCandidateNumber: 14,
@@ -13,8 +14,21 @@ const dashBoardTest = {
   numberThanks: 4,
   candidateCount: [10],
   lastCandidateCount: 5,
-  backgroundColour: "white",
-  dropDownArray: ["A, B, C"],
+  dropDownArray: ["software eng", "mayu's butthole", "brian's face"],
+
+  starStatus: false,
+  userID: 1234,
+  positionTitle: "Sr. Backend Dev",
+  salaryRange: "$110k-140k $/Yr",
+  jobLocation: "Toronto | Ontario",
+  companyLogo: "./images/Google-logo.png",
+  numEmployees: "5000+",
+  companyName: "Google Ltd",
+  stageInterview: "Final Stage",
+  stageNumber: "(5 of 5)",
+  pastPosition1: "Director of Eng.",
+  pastIndustry1: "Finance",
+  standOutSkill1: "Strong Communication",
 };
 
 const DashBoard = () => {
@@ -40,17 +54,98 @@ const DashBoard = () => {
           />
         </div>
       </div>
-      <div className={"grid grids-col-2"}>
+      <div className={"grid grid-cols-2"}>
         <div className={"pl-20"}>
           <CandidateCount
             candidateCount={dashBoardTest.candidateCount}
             lastCandidateCount={dashBoardTest.lastCandidateCount}
           />
         </div>
-        <div>
-          <SearchFilterDash
-            backgroundColour={dashBoardTest.backgroundColour}
-            dropDownArray={dashBoardTest.dropDownArray}
+        <div className={"grid justify-items-end col-start-2 pr-40 py-8"}>
+          <div className={"grid grid-cols-2"}>
+            <div className={"col-star-1"}>Filter Referrals: </div>
+            <div className={"col-start-2"}>
+              <SearchFilterDash dropDownArray={dashBoardTest.dropDownArray} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={"bg-gray-50 px-20"}>
+        <div className={"grid grid-cols-14 gap-4 bg-gray-200 grid-flow-col"}>
+          <div className={"col-start-1 pl-4"}> Referral</div>
+          <div className={"grid-start-2 grid-end-5"}> Role Interviewed For</div>
+          <div className={"grid-start-5 grid-end-7"}>Referring By</div>
+          <div className={"grid-start-7 grid-end-9"}> Vetted To</div>
+          <div className={"grid-start-9 grid-end-12"}>Last Role</div>
+          <div className={"grid-start-12 grid-end-13"}>Stand Out Skill</div>
+          <div className={"grid-start-14"}></div>
+        </div>
+        <div className={"py-4"}>
+          <CandidateTile
+            starStatus={dashBoardTest.starStatus}
+            userID={dashBoardTest.userID}
+            positionTitle={dashBoardTest.positionTitle}
+            salaryRange={dashBoardTest.salaryRange}
+            jobLocation={dashBoardTest.jobLocation}
+            companyLogo={dashBoardTest.companyLogo}
+            numEmployees={dashBoardTest.numEmployees}
+            companyName={dashBoardTest.companyName}
+            stageInterview={dashBoardTest.stageInterview}
+            stageNumber={dashBoardTest.stageNumber}
+            pastPosition1={dashBoardTest.pastIndustry1}
+            pastIndustry1={dashBoardTest.pastIndustry1}
+            standOutSkill1={dashBoardTest.standOutSkill1}
+          />
+        </div>
+        <div className={"py-4"}>
+          <CandidateTile
+            starStatus={dashBoardTest.starStatus}
+            userID={dashBoardTest.userID}
+            positionTitle={dashBoardTest.positionTitle}
+            salaryRange={dashBoardTest.salaryRange}
+            jobLocation={dashBoardTest.jobLocation}
+            companyLogo={dashBoardTest.companyLogo}
+            numEmployees={dashBoardTest.numEmployees}
+            companyName={dashBoardTest.companyName}
+            stageInterview={dashBoardTest.stageInterview}
+            stageNumber={dashBoardTest.stageNumber}
+            pastPosition1={dashBoardTest.pastIndustry1}
+            pastIndustry1={dashBoardTest.pastIndustry1}
+            standOutSkill1={dashBoardTest.standOutSkill1}
+          />
+        </div>
+        <div className={"py-4"}>
+          <CandidateTile
+            starStatus={dashBoardTest.starStatus}
+            userID={dashBoardTest.userID}
+            positionTitle={dashBoardTest.positionTitle}
+            salaryRange={dashBoardTest.salaryRange}
+            jobLocation={dashBoardTest.jobLocation}
+            companyLogo={dashBoardTest.companyLogo}
+            numEmployees={dashBoardTest.numEmployees}
+            companyName={dashBoardTest.companyName}
+            stageInterview={dashBoardTest.stageInterview}
+            stageNumber={dashBoardTest.stageNumber}
+            pastPosition1={dashBoardTest.pastIndustry1}
+            pastIndustry1={dashBoardTest.pastIndustry1}
+            standOutSkill1={dashBoardTest.standOutSkill1}
+          />
+        </div>
+        <div className={"py-4"}>
+          <CandidateTile
+            starStatus={dashBoardTest.starStatus}
+            userID={dashBoardTest.userID}
+            positionTitle={dashBoardTest.positionTitle}
+            salaryRange={dashBoardTest.salaryRange}
+            jobLocation={dashBoardTest.jobLocation}
+            companyLogo={dashBoardTest.companyLogo}
+            numEmployees={dashBoardTest.numEmployees}
+            companyName={dashBoardTest.companyName}
+            stageInterview={dashBoardTest.stageInterview}
+            stageNumber={dashBoardTest.stageNumber}
+            pastPosition1={dashBoardTest.pastIndustry1}
+            pastIndustry1={dashBoardTest.pastIndustry1}
+            standOutSkill1={dashBoardTest.standOutSkill1}
           />
         </div>
       </div>
