a8863dc8df69e6ff3628fe888be1a73b103b6106
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-29 21:30:10 -0400

    stage 1 for now

--- components/CandidateOnboarding/CandidateOnOne.tsx
+++ components/CandidateOnboarding/CandidateOnOne.tsx
@@ -4,24 +4,27 @@ import { ButtonVouch } from "../ui/ButtonVouch";
 export interface CandidateOnOneProps {}
 
 export const CandidateOnOne: React.FC<CandidateOnOneProps> = ({}) => {
+  const nextState = () => {};
   return (
     <div>
-      <div className={"border-2 border-black rounded-xl w-3/5 h-auto"}>
-        <div className={""}>Welcome to a Network of Premium Candidates</div>
-        <div className={"text-xs"}>
+      <div className={"px-8 shadow-lg rounded-xl w-3/5 h-auto bg-white"}>
+        <div className={"pt-8"}>Welcome to a Network of Premium Candidates</div>
+        <div className={"pt-2 text-xs"}>
           To maximize your chances of being match wtih another similar company,
           let the HR Managers learn a little more about you.
         </div>
-        <div className={"border-b border-gray-200 "}></div>
+        <div className={"pb-4 border-b border-gray-200 "}></div>
+        <div className={"flex items-center justify-center pt-4 pb-8"}>
+          <ButtonVouch
+            backgroundColour={"VouchGreen"}
+            buttonType={"rounded"}
+            textColour={"white"}
+            label={"Yes, Sign me up!"}
+            disabled={false}
+            onClick={nextState}
+          />
+        </div>
       </div>
-      <ButtonVouch
-        backgroundColour={"VouchGreen"}
-        buttonType={"rounded"}
-        textColour={"white"}
-        label={"Yes, Sign me up!"}
-        disabled={false}
-        onClick={}
-      />
     </div>
   );
 };
