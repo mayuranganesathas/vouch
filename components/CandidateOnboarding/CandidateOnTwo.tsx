b4b527b339b5ba014e0ddc5284a0141d0c509ac3
brianghub <87824467+brianghub@users.noreply.github.com>
2021-10-31 10:56:59 -0400

    second stage

---
+++ components/CandidateOnboarding/CandidateOnTwo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ButtonVouch } from "../ui/ButtonVouch";
+
+export interface CandidateOnTwoProps {}
+
+export const CandidateOnTwo: React.FC<CandidateOnTwoProps> = ({}) => {
+  const nextState = () => {};
+  return (
+    <div>
+      <div className={"px-8 shadow-lg rounded-xl w-3/5 h-auto bg-white"}>
+        <div className={"pt-8"}>Welcome to a Network of Premium Candidates</div>
+        <div className={"pt-2 text-xs"}>
+          To maximize your chances of being match wtih another similar company,
+          let the HR Managers learn a little more about you.
+        </div>
+        <div className={"pb-4 border-b border-gray-200 "}></div>
+        <div className={"flex items-center justify-center pt-4 pb-8"}>
+          <ButtonVouch
+            backgroundColour={"VouchGreen"}
+            buttonType={"rounded"}
+            textColour={"white"}
+            label={"Yes, Sign me up!"}
+            disabled={false}
+            onClick={nextState}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/stories/CandidateOnTwo.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import {
+  CandidateOnTwo,
+  CandidateOnTwoProps,
+} from "../CandidateOnboarding/CandidateOnTwo";
+
+export default {
+  title: "CandidateOnBoarding/CandidateOnTwo",
+  cinoibebt: CandidateOnTwo,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<CandidateOnTwoProps> = (args) => (
+  <CandidateOnTwo {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
