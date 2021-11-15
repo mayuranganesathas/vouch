f4d7ba128bf202f9b3b7fe4202e38f396b2720d1
brianghub <87824467+brianghub@users.noreply.github.com>
2021-11-14 19:24:20 -0500

    Home Button Nav

--- components/dashView/UserIdBar.tsx
+++ components/dashView/UserIdBar.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import UserProfile from "./UserProfile";
 import HrCompanyProfile from "./HrCompanyProfile";
+import { ButtonNav } from "../ui/ButtonNav";
+
+export interface UserIdBarProps {
+  Home: boolean;
+  setHome: (Home: boolean) => false;
+}
+
+const HomeData = {
+  backgroundColour: "white",
+  textColour: "black",
+};
 
 const UserIdBarTest = {
   userHrCompanyName: "Google HQ",
@@ -12,20 +23,48 @@ const UserIdBarTest = {
   userHrPosition: "HR Lead",
 };
 
-const UserIdBar = () => {
+const UserIdBar = ({ Home, setHome }: UserIdBarProps) => {
+  const HomeActive = () => {
+    setHome(true);
+  };
   return (
-    <div className="flex px-2 justify-between">
-      <HrCompanyProfile
-        userHrCompanyName={UserIdBarTest.userHrCompanyName}
-        userHrCompanyImage={UserIdBarTest.userHrCompanyImage}
-        userHrCompanyIndustry={UserIdBarTest.userHrCompanyIndustry}
-        userHrCompanyWebsite={UserIdBarTest.userHrCompanyWebsite}
-      />
-      <UserProfile
-        userHrImg={UserIdBarTest.userHrImg}
-        userHrName={UserIdBarTest.userHrName}
-        userHrPosition={UserIdBarTest.userHrPosition}
-      />
+    <div className="grid grid-cols-14 grid-flow-col">
+      <div className="grid-start-1 grid-end-3">
+        <HrCompanyProfile
+          userHrCompanyName={UserIdBarTest.userHrCompanyName}
+          userHrCompanyImage={UserIdBarTest.userHrCompanyImage}
+          userHrCompanyIndustry={UserIdBarTest.userHrCompanyIndustry}
+          userHrCompanyWebsite={UserIdBarTest.userHrCompanyWebsite}
+        />
+      </div>
+      <div className="grid-start-4 grid-end-5 flex items-center">
+        <ButtonNav
+          backgroundColour="OnClick"
+          buttonType={"square"}
+          textColour="VouchGreen"
+          label={"Home"}
+          disabled={false}
+          onClick={HomeActive}
+          buttonWidth={"wide"}
+        />{" "}
+      </div>
+      <div className="grid-start-5 grid-end-6 flex items-center">Favourite</div>
+      <div className="grid-start-6 grid-end-7 flex items-center"> Not Now</div>
+      <div className="grid-start-7 grid-end-8 flex items-center">
+        {" "}
+        Connected{" "}
+      </div>
+      <div className="grid-start-8 grid-end-9"></div>
+      <div className="grid-start-9 grid-end-10"></div>
+      <div className="grid-start-10 grid-end-11"> </div>
+      <div className="grid-start-11 grid-end-12"></div>
+      <div className="grid-start-12 grid-end-14 justify-items-end">
+        <UserProfile
+          userHrImg={UserIdBarTest.userHrImg}
+          userHrName={UserIdBarTest.userHrName}
+          userHrPosition={UserIdBarTest.userHrPosition}
+        />
+      </div>
     </div>
   );
 };
---
+++ components/ui/ButtonNav.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+export interface ButtonNavProps {
+  backgroundColour: "OnClick" | "white";
+  buttonType: "rounded" | "square";
+  textColour: "black" | "VouchGreen";
+  label: string;
+  disabled?: boolean;
+  onClick: () => void;
+  buttonWidth?: "wide";
+}
+
+export const ButtonNav: React.FC<ButtonNavProps> = ({
+  backgroundColour,
+  buttonType,
+  textColour,
+  label,
+  disabled,
+  onClick,
+  buttonWidth,
+}) => {
+  let buttonSize;
+
+  switch (buttonWidth) {
+    case "wide":
+      buttonSize = "h-full w-full";
+      break;
+  }
+  let backgroundStyles;
+  switch (backgroundColour) {
+    case "OnClick":
+      backgroundStyles = "bg-gray-100";
+      break;
+
+    case "white":
+      backgroundStyles = "bg-white";
+      break;
+  }
+  let buttonStyles;
+  switch (buttonType) {
+    case "rounded":
+      buttonStyles = "rounded-full";
+      break;
+    case "square":
+      buttonStyles = "rounded";
+  }
+
+  let textStyles;
+  switch (textColour) {
+    case "black":
+      textStyles = "text-black";
+      break;
+    case "VouchGreen":
+      textStyles = "text-VouchGreen";
+      break;
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      type="button"
+      className={`bg-gradient-to-b text-red font-bold ${buttonSize}
+      ${disabled ? "bg-gray-400 opacity-25" : backgroundStyles}
+      ${disabled ? "rounded" : buttonStyles}
+      ${disabled ? "" : "active:border-b-2"}
+      ${disabled ? "cursor-default" : "cursor-pointer"}
+      `}
+    >
+      <div className={textStyles}>{label} </div>
+    </button>
+  );
+};
