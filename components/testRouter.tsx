8b9304921d6b2cc393a0c2b5f58a24fbdfe6881c
Mayuran Ganesathas <mayuran.ganesathas@gmail.com>
2021-11-15 18:01:03 -0500

    mg

---
+++ components/testRouter.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Modal from "react-modal";
+import { useRouter } from "next/router";
+
+export interface RouterListProps {}
+const RouterList = ({}: RouterListProps) => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <div className="p-8 ">
+        <button
+          onClick={() => router.push("/pages")}
+          className="bg-VouchGreen text-white text-center"
+        >
+          {" "}
+          Visit Dashboard{" "}
+        </button>
+      </div>
+      <div className="p-8 ">
+        <button
+          onClick={() => router.push("/candidate-register")}
+          className="bg-VouchGreen text-white text-center"
+        >
+          {" "}
+          Visit Candidate Register{" "}
+        </button>
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default RouterList;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-
-
 export default function Home() {
   return (
     <div>
-     
-the beginning of BNB !   </div>
-  )
+      the beginning of BNB !
+      <RouterList />{" "}
+    </div>
+  );
 }
