577a4a3c96059ea91ba19b37ad33978e75177f62
Mayu <37229329+mayuranganesathas@users.noreply.github.com>
2021-11-15 17:25:04 -0500

    Revert "Create Candidates Navigation Bar"

--- components/dashView/UserIdBar.tsx
+++ components/dashView/UserIdBar.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import UserProfile from "./UserProfile";
 import HrCompanyProfile from "./HrCompanyProfile";
-import { ButtonNav } from "../ui/ButtonNav";
-
-const HomeData = {
-  backgroundColour: "white",
-  textColour: "black",
-};
 
 const UserIdBarTest = {
   userHrCompanyName: "Google HQ",
@@ -18,95 +12,20 @@ const UserIdBarTest = {
   userHrPosition: "HR Lead",
 };
 
-const UserIdBar = (
-  homeActive: boolean,
-  setHomeActive: (homeActive: boolean) => true,
-  favouriteActive: boolean,
-  setFavouriteActive: (favouriteActive: boolean) => true,
-  notNowActive: boolean,
-  setNotNowActive: (notNowActive: boolean) => true,
-  connectedActive: boolean,
-  setConnectedActive: (connectedActive: boolean) => true
-) => {
-  const HomeActive = () => {
-    setHomeActive(false);
-  };
-  const FavouriteActive = () => {
-    setFavouriteActive(false);
-  };
-  const NotNowActive = () => {
-    setNotNowActive(false);
-  };
-  const ConnectedActive = () => {
-    setConnectedActive(false);
-  };
+const UserIdBar = () => {
   return (
-    <div className="grid grid-cols-14 grid-flow-col">
-      <div className="grid-start-1 grid-end-3">
-        <HrCompanyProfile
-          userHrCompanyName={UserIdBarTest.userHrCompanyName}
-          userHrCompanyImage={UserIdBarTest.userHrCompanyImage}
-          userHrCompanyIndustry={UserIdBarTest.userHrCompanyIndustry}
-          userHrCompanyWebsite={UserIdBarTest.userHrCompanyWebsite}
-        />
-      </div>
-      <div className="grid-start-4 grid-end-5 flex items-center">
-        <ButtonNav
-          backgroundColour={`${homeActive ? "white" : "onClick"}`}
-          buttonType={"square"}
-          textColour={`${homeActive ? "black" : "VouchGreen"}`}
-          label={"Home"}
-          disabled={false}
-          onClick={HomeActive}
-          buttonWidth={"wide"}
-        />{" "}
-      </div>
-      <div className="grid-start-5 grid-end-6 flex items-center">
-        {" "}
-        <ButtonNav
-          backgroundColour={`${favouriteActive ? "white" : "onClick"}`}
-          buttonType={"square"}
-          textColour={`${favouriteActive ? "black" : "VouchGreen"}`}
-          label={"Favourite"}
-          disabled={false}
-          onClick={FavouriteActive}
-          buttonWidth={"wide"}
-        />
-      </div>
-      <div className="grid-start-6 grid-end-7 flex items-center">
-        {" "}
-        <ButtonNav
-          backgroundColour={`${notNowActive ? "white" : "onClick"}`}
-          buttonType={"square"}
-          textColour={`${notNowActive ? "black" : "VouchGreen"}`}
-          label={"Not Now"}
-          disabled={false}
-          onClick={NotNowActive}
-          buttonWidth={"wide"}
-        />
-      </div>
-      <div className="grid-start-7 grid-end-8 flex items-center">
-        <ButtonNav
-          backgroundColour={`${connectedActive ? "white" : "onClick"}`}
-          buttonType={"square"}
-          textColour={`${connectedActive ? "black" : "VouchGreen"}`}
-          label={"Connected"}
-          disabled={false}
-          onClick={ConnectedActive}
-          buttonWidth={"wide"}
-        />
-      </div>
-      <div className="grid-start-8 grid-end-9"></div>
-      <div className="grid-start-9 grid-end-10"></div>
-      <div className="grid-start-10 grid-end-11"> </div>
-      <div className="grid-start-11 grid-end-12"></div>
-      <div className="grid-start-12 grid-end-14 justify-items-end">
-        <UserProfile
-          userHrImg={UserIdBarTest.userHrImg}
-          userHrName={UserIdBarTest.userHrName}
-          userHrPosition={UserIdBarTest.userHrPosition}
-        />
-      </div>
+    <div className="flex px-2 justify-between">
+      <HrCompanyProfile
+        userHrCompanyName={UserIdBarTest.userHrCompanyName}
+        userHrCompanyImage={UserIdBarTest.userHrCompanyImage}
+        userHrCompanyIndustry={UserIdBarTest.userHrCompanyIndustry}
+        userHrCompanyWebsite={UserIdBarTest.userHrCompanyWebsite}
+      />
+      <UserProfile
+        userHrImg={UserIdBarTest.userHrImg}
+        userHrName={UserIdBarTest.userHrName}
+        userHrPosition={UserIdBarTest.userHrPosition}
+      />
     </div>
   );
 };
