5867f0e42df987ccd1c47d64dceea6cbbbdb7a7b
brianghub <87824467+brianghub@users.noreply.github.com>
2021-11-18 23:49:04 -0500

    Revert "Merge branch 'main' into BL-CandidateTileFilterIconsUpdate"
    This reverts commit 3591b257ee08b6ee36aac040b0d67c018ac8389f, reversing
    changes made to 128412fee875d30178b874d97e829f8f56f748c9.

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,15 @@
 import "tailwindcss/tailwind.css";
 import initializeApollo from "../lib/apollo";
 import { ApolloProvider } from "@apollo/client";
-import { AuthProvider, useAuth } from "../lib/authContext";
 
 function MyApp({ Component, pageProps }) {
   const client = initializeApollo();
 
   return (
-    <AuthProvider>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
 
 export default MyApp;
-
-// function Auth({ children }) {
-//   const { user, loading } = useAuth();
-//   const router = useRouter();
-
-//   React.useEffect(() => {
-//     if (loading) return; // Do nothing while loading
-//     if (!user) router.push("/welcome"); // If not authenticated, force log in
-//   }, [user, loading]);
-
-//   if (user) {
-//     return children;
-//   }
-
-//   // Session is being fetched, or no user.
-//   // If no user, useEffect() will redirect.
-//   return <div>Loading...</div>;
-// }
