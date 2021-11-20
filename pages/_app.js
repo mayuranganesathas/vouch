77ff6b56a718d32edf3f8dfeb10bc8c9136bbf14
Mayuran Ganesathas <mayuran.ganesathas@gmail.com>
2021-11-20 14:18:28 -0500

    mg

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import "tailwindcss/tailwind.css";
 import initializeApollo from "../lib/apollo";
 import { ApolloProvider } from "@apollo/client";
 import { AuthProvider, useAuth } from "../lib/authContext";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   const client = initializeApollo();
@@ -9,7 +11,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
       </ApolloProvider>
     </AuthProvider>
   );
@@ -17,20 +21,20 @@ function MyApp({ Component, pageProps }) {
 
 export default MyApp;
 
-// function Auth({ children }) {
-//   const { user, loading } = useAuth();
-//   const router = useRouter();
+function Auth({ children }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
 
-//   React.useEffect(() => {
-//     if (loading) return; // Do nothing while loading
-//     if (!user) router.push("/welcome"); // If not authenticated, force log in
-//   }, [user, loading]);
+  React.useEffect(() => {
+    if (loading) return; // Do nothing while loading
+    if (!user) router.push("/signup"); // If not authenticated, force log in
+  }, [user, loading]);
 
-//   if (user) {
-//     return children;
-//   }
+  if (user) {
+    return children;
+  }
 
-//   // Session is being fetched, or no user.
-//   // If no user, useEffect() will redirect.
-//   return <div>Loading...</div>;
-// }
+  // Session is being fetched, or no user.
+  // If no user, useEffect() will redirect.
+  return <div>Loading...</div>;
+}
