19e91f79d38edcc585a6716fa1b7c2da80eeecd9
brianghub <87824467+brianghub@users.noreply.github.com>
2021-11-20 16:24:47 -0500

    columns

--- components/HROnboarding/HROnboarding.tsx
+++ components/HROnboarding/HROnboarding.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ButtonVouch } from "../ui/ButtonVouch";
+import { SearchFilterDash } from "../ui/searchFilterDash";
 
 export interface HROnboardingProps {
   onClick: () => void;
@@ -8,15 +9,23 @@ export interface HROnboardingProps {
 export const HROnboarding: React.FC<HROnboardingProps> = ({ onClick }) => {
   return (
     <div className={"flex justify-center items-center"}>
-      <div className={"px-8 shadow-lg rounded-xl w-3/5 h-auto bg-white"}>
+      <div className={"px-8 shadow-lg rounded-xl w-2/5 h-auto bg-white"}>
         <div className={"pt-8 text-left"}>Set up your Company Profile</div>
         <div className={"border-b-2 border-gray-300 py-2"}> </div>
-        <div className={"pt-2 text-xs text-left"}>
+        <div className={"py-2 text-xs text-left"}>
           Thank you for joining vouch. In order to properly match referred
           candidates, we need a little information about your company. You and
           other HR Managers will be able to see candidate details as well as the
           company details where they were referred from to help you qualify the
           candidates.
+        </div>
+        <div className={"grid grid-cols-2 gap-2"}></div>
+        <div className={"col-start-1 pt-4"}>
+          <div className={"text-sm"}> Company Name:</div>
+          <input
+            className={"border border-gray-300 text-xs rounded px-4"}
+            placeholder="Enter company name"
+          ></input>
         </div>
         <div className="flex justify-center py-4">
           <ButtonVouch
