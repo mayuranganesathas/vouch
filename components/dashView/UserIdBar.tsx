f925f3f5d920e851a23c6dcccf3c3e1238ef8de7
brianghub <87824467+brianghub@users.noreply.github.com>
2021-11-23 20:38:40 -0500

    nav bar layout

--- components/dashView/UserIdBar.tsx
+++ components/dashView/UserIdBar.tsx
@@ -17,18 +17,41 @@ const UserIdBar = () => {
   const { user } = useAuth();
 
   return (
-    <div className="flex px-2 justify-between">
-      <HrCompanyProfile
-        userHrCompanyName={UserIdBarTest.userHrCompanyName}
-        userHrCompanyImage={UserIdBarTest.userHrCompanyImage}
-        userHrCompanyIndustry={UserIdBarTest.userHrCompanyIndustry}
-        userHrCompanyWebsite={UserIdBarTest.userHrCompanyWebsite}
-      />
-      <UserProfile
-        userHrImg={user.photoURL}
-        userHrName={user.displayName}
-        userHrPosition={UserIdBarTest.userHrPosition}
-      />
+    <div className={"grid grid-cols-12"}>
+      <div className={"col-start-1 col-span-2"}>
+        <div className="flex px-2 justify-between">
+          <HrCompanyProfile
+            userHrCompanyName={UserIdBarTest.userHrCompanyName}
+            userHrCompanyImage={UserIdBarTest.userHrCompanyImage}
+            userHrCompanyIndustry={UserIdBarTest.userHrCompanyIndustry}
+            userHrCompanyWebsite={UserIdBarTest.userHrCompanyWebsite}
+          />
+        </div>
+      </div>
+      <div className={"col-start-4 flex justify-center items-center"}>
+        {" "}
+        Home{" "}
+      </div>
+      <div className={"col-start-5 flex justify-center items-center"}>
+        {" "}
+        Favourites
+      </div>
+      <div className={"col-start-6 flex justify-center items-center"}>
+        {" "}
+        Not now{" "}
+      </div>
+      <div className={"col-start-7 flex justify-center items-center"}>
+        {" "}
+        Contacted{" "}
+      </div>
+
+      <div className={"col-start-11 col-span-2"}>
+        <UserProfile
+          userHrImg={user.photoURL}
+          userHrName={user.displayName}
+          userHrPosition={UserIdBarTest.userHrPosition}
+        />
+      </div>
     </div>
   );
 };
