1cda668a2c9dadb22e0506a2f893469904dc4e4e
Mayuran Ganesathas <mayuran.ganesathas@gmail.com>
2021-11-30 15:33:25 -0500

    mg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   env: {
     HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET,
