fffea79b99401ef5b0b6a10c812c8511488b8dfa
brianghub <87824467+brianghub@users.noreply.github.com>
2021-12-20 14:14:08 -0500

    colour update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: { colors: { VouchGreen: "#49BEB0" } },
+    extend: { colors: { VouchGreen: "#49BEB0", VouchDark: "#305252" } },
   },
   variants: {
     extend: {},
