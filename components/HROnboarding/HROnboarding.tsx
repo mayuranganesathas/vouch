babd90de1e9ab37263a29541df4785173de6003f
brianghub <87824467+brianghub@users.noreply.github.com>
2021-12-24 14:33:42 -0500

    right column relayered

--- components/HROnboarding/HROnboarding.tsx
+++ components/HROnboarding/HROnboarding.tsx
@@ -80,88 +80,109 @@ export const HROnboarding: React.FC<HROnboardingProps> = ({
   ];
   return (
     <div className={"flex justify-center items-center"}>
-      <div className={"px-8 shadow-lg rounded-xl w-2/5 h-auto bg-white"}>
-        <div className={"pt-8 text-left"}>Set up your Company Profile</div>
-        <div className={"border-b-2 border-gray-300 py-2"}> </div>
-        <div className={"py-2 text-xs text-left"}>
-          Thank you for joining vouch. In order to properly match referred
-          candidates, we need a little information about your company. You and
-          other HR Managers will be able to see candidate details as well as the
-          company details where they were referred from to help you qualify the
-          candidates.
-        </div>
-        <div className={"grid grid-cols-2 gap-2"}>
-          <div className={"col-start-1 pt-4"}>
-            <div>
-              <div className={"text-sm"}> Company Name:</div>
-              <input
-                className={
-                  "border border-gray-300 text-xs rounded py-2 px-4 w-4/5"
-                }
-                placeholder="Enter company name"
-                value={hrVoucherCompanyName}
-                onChange={(e) => setHrVoucherCompanyName(e.target.value)}
-              />
-            </div>
-
-            <div className={"py-4"}>
-              <div className={"text-sm"}> Company Website:</div>
-              <input
-                className={
-                  "border border-gray-300 text-xs rounded py-2 px-4 w-4/5"
-                }
-                placeholder="Paste your company's website URL"
-                value={hrVoucherCompanyWebsite}
-                onChange={(e) => setHrVoucherWebsite(e.target.value)}
-              />
-            </div>
-            <div>
-              <div className={"text-sm"}> Location:</div>
-              <input
-                className={
-                  "border border-gray-300 text-xs rounded py-2 px-4 w-4/5"
-                }
-                placeholder="Enter the city for your Corporate HQ"
-                value={hrLocation}
-                onChange={(e) => setHrLocation(e.target.value)}
-              />
-            </div>
+      <div className={"shadow-lg rounded-xl w-3/6 h-auto bg-white"}>
+        <div className={"grid grid-cols-3"}>
+          <div className={"col-start-1 bg-VouchMed rounded-l-lg"}>
+            <div></div>
           </div>
-          <div className={"col-start-2 pt-4"}>
-            <div>
-              <div className={"text-sm"}>Industry</div>
-              <SearchFilterDash
-                backgroundColour={"white"}
-                dropDownArray={industryArray1}
-                value={industryArray}
-                onChange={(e) => {
-                  setIndustryArray(e.target.value);
-                }}
-              />
+          <div className={"col-start-2 col-span-2 px-8"}>
+            <div className={"pt-8 text-left font-bold text-base"}>
+              Set Up Your Company Profile
             </div>
-            <div className={"py-4"}>
-              <div className={"text-sm"}>Company Size</div>
-              <SearchFilterDash
-                backgroundColour={"white"}
-                dropDownArray={employeeArray1}
-                value={employeeArray}
-                onChange={(e) => {
-                  setEmployeeArray(e.target.value);
-                }}
-              />
-              <div className={"text-sm"}>Your Position</div>
-              <SearchFilterDash
-                backgroundColour={"white"}
-                dropDownArray={hrPositionArray1}
-                value={hrVoucherPosition}
-                onChange={(e) => {
-                  setHrVoucherPosition(e.target.value);
-                }}
-              />
+            <div className={"text-gray-600 text-sm pt-1"}>
+              Thank you for joining Vouch
             </div>
+            <div className={"text-gray-600 text-sm py-4"}>
+              Please take a moment to set up your profile. You and other HR
+              Leaders will be able to view candidate details and the company
+              details of other organizations who have referred candidates into
+              the platform.{" "}
+            </div>
+            <div className={"text-gray-600 text-sm py-2"}>
+              How your dashboard will look:
+            </div>
+            <div className={"px-8"}>
+              <img src="./images/tileExample.png" width="full" height="auto" />
+            </div>
+            <div className="py-6">
+              <hr className="" />
+            </div>
+            <div className={"font-bold text-base"}>Company Details</div>
+            <div className={"grid grid-cols-2 gap-2"}>
+              <div className={"col-start-1 pt-4"}>
+                <div className={"pt-4"}>
+                  <input
+                    className={
+                      "border border-gray-300 text-xs rounded py-2 px-4 w-4/5"
+                    }
+                    placeholder="Enter company name"
+                    value={hrVoucherCompanyName}
+                    onChange={(e) => setHrVoucherCompanyName(e.target.value)}
+                  />
+                </div>
 
-            <div>
-              {/* <div className={"text-sm pb-4"}>Upload your company's logo:</div>
+                <div className={"py-8"}>
+                  <input
+                    className={
+                      "border border-gray-300 text-xs rounded py-2 px-4 w-4/5"
+                    }
+                    placeholder="Website - Paste your company URL"
+                    value={hrVoucherCompanyWebsite}
+                    onChange={(e) => setHrVoucherWebsite(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <input
+                    className={
+                      "border border-gray-300 text-xs rounded py-2 px-4 w-4/5"
+                    }
+                    placeholder="Location - City of your Corporate HQ"
+                    value={hrLocation}
+                    onChange={(e) => setHrLocation(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className={"col-start-2 pt-4"}>
+                <div>
+                  <div className={"text-xs font-bold text-gray-500"}>
+                    Industry
+                  </div>
+                  <SearchFilterDash
+                    backgroundColour={"white"}
+                    dropDownArray={industryArray1}
+                    value={industryArray}
+                    onChange={(e) => {
+                      setIndustryArray(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className={"py-4"}>
+                  <div className={"text-xs font-bold text-gray-500"}>
+                    Company Size
+                  </div>
+                  <SearchFilterDash
+                    backgroundColour={"white"}
+                    dropDownArray={employeeArray1}
+                    value={employeeArray}
+                    onChange={(e) => {
+                      setEmployeeArray(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className={"text-xs font-bold text-gray-500"}>
+                  Your Position
+                </div>
+                <SearchFilterDash
+                  backgroundColour={"white"}
+                  dropDownArray={hrPositionArray1}
+                  value={hrVoucherPosition}
+                  onChange={(e) => {
+                    setHrVoucherPosition(e.target.value);
+                  }}
+                />
+
+                <div>
+                  {/* <div className={"text-sm pb-4"}>Upload your company's logo:</div>
               <form action="/action_page.php" className={"text-xs"}>
                 <input
                   type="file"
@@ -171,43 +192,45 @@ export const HROnboarding: React.FC<HROnboardingProps> = ({
                   onChange={(e) => setHrCompanyLogo(e.target.value)}
                 />
               </form> */}
-            </div>
-          </div>
-        </div>
-        <div className={"grid grid-cols-2"}>
-          <div className={"col-start-1"}>
-            <div className={"flex content-end pt-20"}>
-              <form action="/action_page.php" className={"text-xs"}>
-                <input
-                  type="checkbox"
-                  id="USCAN"
-                  name="USCANVerified"
-                  checked={checkBoxValidation}
-                  onChange={(e) => setCheckBoxValidation(e.target.checked)}
-                />
-              </form>
-
-              <div className={"text-xs pl-2"}>
-                {" "}
-                We recruit for roles in Canada and USA
+                </div>
               </div>
             </div>
-          </div>
-          <div className={"col-start-2"}>
-            <div className="flex justify-center pt-14">
-              <ButtonVouch
-                backgroundColour={"VouchGreen"}
-                buttonType={"rounded"}
-                textColour={"white"}
-                label={"Submit"}
-                disabled={formValidation()}
-                onClick={onClick}
-              />
+            <div className={"grid grid-cols-2"}>
+              <div className={"col-start-1"}>
+                <div className={"flex content-end pt-20"}>
+                  <form action="/action_page.php" className={"text-xs"}>
+                    <input
+                      type="checkbox"
+                      id="USCAN"
+                      name="USCANVerified"
+                      checked={checkBoxValidation}
+                      onChange={(e) => setCheckBoxValidation(e.target.checked)}
+                    />
+                  </form>
+
+                  <div className={"text-xs pl-2"}>
+                    {" "}
+                    We recruit for roles in Canada and USA
+                  </div>
+                </div>
+              </div>
+              <div className={"col-start-2"}>
+                <div className="flex justify-center pt-14">
+                  <ButtonVouch
+                    backgroundColour={"VouchGreen"}
+                    buttonType={"rounded"}
+                    textColour={"white"}
+                    label={"Submit"}
+                    disabled={formValidation()}
+                    onClick={onClick}
+                  />
+                </div>
+              </div>
             </div>
+
+            <div className={"pb-4 border-b border-gray-200 "}></div>
           </div>
         </div>
-
-        <div className={"pb-4 border-b border-gray-200 "}></div>
       </div>
     </div>
   );
---
+++ components/stories/HROnboarding.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { HROnboarding, HROnboardingProps } from "../HROnboarding/HROnboarding";
+
+export default {
+  title: "Dashboard/HROnboarding",
+  component: HROnboarding,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<HROnboardingProps> = (args) => <HROnboarding {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  industryArray: ["damn", "you"],
+  employeeArray: ["damn", "you"],
+  hrVoucherPosition: ["damn", "you"],
+  checkBoxValidation: true,
+  formValidation: true,
+};
