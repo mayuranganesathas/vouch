37bd5d425a54137ef2749e2e9ff514bfe282b530
Mayuran Ganesathas <mayuran.ganesathas@gmail.com>
2022-01-05 21:21:02 -0500

    mg

--- components/dashView/CandidateTile.tsx
+++ components/dashView/CandidateTile.tsx
@@ -171,12 +171,12 @@ export const CandidateTile: React.FC<CandidateTileProps> = ({
 
             <div className={"col-start-2 pt-4 pl-4"}>
               <div>
-                <p className={"w-full text-base border-r-4 border-gray-100 "}>
+                <p className={"w-full text-base border-r-4 border-gray-300 "}>
                   {pastPosition1}
                 </p>
               </div>
               <div>
-                <p className={"w-full pt-1 border-r-4 border-gray-100"}>
+                <p className={"w-full pt-1 border-r-4 border-gray-300"}>
                   in {pastIndustry1}
                 </p>
               </div>
