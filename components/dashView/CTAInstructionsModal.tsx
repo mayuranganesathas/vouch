84f55e24a3c26d9eca000c9f8fac00b917a3a40a
brianghub <87824467+brianghub@users.noreply.github.com>
2022-02-10 11:58:58 -0500

    modelception

--- components/dashView/CTAInstructionsModal.tsx
+++ components/dashView/CTAInstructionsModal.tsx
@@ -4,6 +4,8 @@ import { UserGroupIcon } from "@heroicons/react/solid";
 import { MailOpenIcon } from "@heroicons/react/solid";
 import { UserAddIcon } from "@heroicons/react/solid";
 import { SearchCircleIcon } from "@heroicons/react/solid";
+import { InformationCircleIcon, XIcon } from "@heroicons/react/solid";
+
 
 export interface IconModalProps{
     modalIsOpen: boolean;
@@ -26,19 +28,25 @@ const IconModal = ({ modalIsOpen, closeModal }: IconModalProps) => {
           }}
         >
           <div>
-          <div className={"text-xs"}>
-               
+          <div className={"text-xs px-6"}>
+            <div className="flex justify-end">     
+              <XIcon
+              className="h-4 w-4 text-gray-400 hover:text-red-500 cursor-pointer "
+              onClick={closeModal}
+            />   
+            </div>   
                 <div
                   className={
-                    "flex flex-nowrap justify-center py-2 font-bold"
+                    "flex flex-nowrap justify-left py-4 font-bold text-sm"
                   }
                 >
-                  VOUCHING CANDIDATES <UserGroupIcon className={"w-4 h-auto text-VouchGreen pl-0.5 "}  />
+                  VOUCHING CANDIDATES 
 
                 </div>
                 <div className={" "}>
-                  <div className={"flex justify-center"}>
-                    Help other qualified candidates from your recruitment pipeline get noticed! They may not have been your final pick, but they could be someone elses.
+                <UserGroupIcon className={"w-6 h-auto text-VouchGreen py-2 "}  />
+                  <div className={"flex justify-left flex-nowrap pb-6"}>
+                  Help other qualified candidates from your recruitment pipeline get noticed! They may not have been your final pick, but they could be someone elses.
 
                   </div>
                  
@@ -46,26 +54,28 @@ const IconModal = ({ modalIsOpen, closeModal }: IconModalProps) => {
                  
                 </div>
                 
-                <div className={"  py-2 flex justify-center flex-nowrap"}>
-                  Candidates get invited to join.<MailOpenIcon className={"w-4 h-auto text-VouchGreen pl-0.5"} />
+                <MailOpenIcon className={"w-6 h-auto text-VouchGreen py-2"} />
+                <div className={"  pb-6 flex justify-left flex-nowrap"}>
+                  Candidates get invited to join.
 
                 </div>
                 
-                <div className={" py-2"}>
-                  <div className={"flex justify-center"}>
+                <div className={""}>
+                <UserAddIcon className={"w-6 h-auto text-VouchGreen py-2"}  />
+
+                  <div className={"flex justify-left pb-6"}>
                     Referred candidates match more easily with our community of recruiters.
-<UserAddIcon className={"w-4 h-auto text-VouchGreen pb-0.5"}  />
 
                   </div>
                   
                 </div>
-                <div className={"flex justify-center pt-8"}>
-                </div>
+               
                 <div>
-                  <div className={" py-2 flex-nowrap"}>
-                    <div className={"flex justify-center"}><SearchCircleIcon className={"w-8 h-auto text-VouchGreen"} />
-Pay it forward. Find active and qualified candidates for your next hire.
-</div>
+                  <div className={""}>
+                  <SearchCircleIcon className={"w-6 h-auto text-VouchGreen py-2"} />
+                    <div className={"flex justify-left pb-6"}>
+                      Pay it forward. Find active and qualified candidates for your next hire.
+                  </div>
                     
                   </div>
                 </div>
