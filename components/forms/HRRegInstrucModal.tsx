c3c1f5ff204325abd892f2f7b45b3a817a8fe323
brianghub <87824467+brianghub@users.noreply.github.com>
2022-02-17 11:01:33 -0500

    final changes

--- components/forms/HRRegInstrucModal.tsx
+++ components/forms/HRRegInstrucModal.tsx
@@ -21,7 +21,7 @@ const IconModal = ({ modalIsOpen, closeModal }: IconModalProps) => {
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        className="  w-1/4 h-3/5 bg-white shadow-lg rounded-xl p-2 m-8  overflow-auto absolute inset-2/4 -top-1"
+        className="  w-1/4 h-auto bg-white shadow-lg rounded-xl p-2 m-8  overflow-auto absolute inset-2/4 -top-1"
         contentLabel="Test Name"
         ariaHideApp={false}
         aria={{
@@ -37,43 +37,53 @@ const IconModal = ({ modalIsOpen, closeModal }: IconModalProps) => {
                 onClick={closeModal}
               />
             </div>
-            <div className={"py-10 text-xs"}>
-              <div className={"flex justify-center"}>
-                <FlagIcon className={"w-8 h-auto "} fill="white" />
-              </div>
+            <div className={"py-2 text-xs"}>
               <div
-                className={
-                  "flex justify-center py-2 font-bold text-sm text-white"
-                }
+                className={"flex justify-start font-bold text-sm text-gray-600"}
               >
                 JOIN THE MOVEMENT
               </div>
-              <div className={" text-white w-full text-center px-6"}>
+              <div className={" text-gray-006 w-full text-start"}>
                 End the war for talent by working together. Gain access to a
                 network of candidates of our very own
               </div>
-              <div className={"flex justify-center pt-12"}>
-                <LockOpenIcon className={"w-8 h-auto"} fill="white" />
+              <div className={"flex flex-nowrap justify-start pt-4"}>
+                <div>
+                  <LockOpenIcon className={"w-8 h-auto text-VouchGreen"} />
+                </div>
+                <div
+                  className={"text-gray-600  w-full pt-2 pb-4 pl-2 text-start"}
+                >
+                  Get access to qualified and active candidates and continuously
+                  build up your pipeline{" "}
+                </div>
               </div>
-              <div className={"text-white  w-full px-6 pt-2 pb-12 text-center"}>
-                Get access to qualified and active candidates and continuously
-                build up your pipeline{" "}
+
+              <div className={"flex flex-nowrap justify-start pt-2"}>
+                <div>
+                  <UserAddIcon className={"w-8 h-auto text-VouchGreen"} />
+                </div>
+                <div
+                  className={" text-gray-600 text-start-full pt-2 pb-4 pl-2"}
+                >
+                  Add late stage candidates from your own pipeline and make it a
+                  win-win!
+                </div>
               </div>
-              <div className={"flex justify-center pt-8"}>
-                <UserAddIcon className={"w-8 h-auto"} fill="white" />
-              </div>
-              <div className={" text-white text-center px-6 w-full pt-2 pb-12"}>
-                Add late stage candidates from your own pipeline and make it a
-                win-win!
-              </div>
-              <div className={"flex justify-center pt-8"}>
-                <TagIcon className={"w-8 h-auto"} fill="white" />
-              </div>
-              <div>
-                <div className={" text-white py-2 width-full text-center px-6"}>
+
+              <div className={"flex flex-nowrap justify-start pt-2"}>
+                <div>
+                  <TagIcon className={"w-8 h-auto text-VouchGreen"} />
+                </div>
+                <div
+                  className={
+                    " text-gray-600 py-2 width-full text-start pb-4 pl-2 "
+                  }
+                >
                   Use Vouch to save time and money on your next hire
                 </div>
               </div>
+              <div></div>
             </div>
           </div>
         </div>
